8f85c8e9765dd07ac5fce9fa8ec834aab1fb5679
Jonathan Schear <jonathanschear@gmail.com>
2023-06-13 20:21:14 -0400

    Include full post content in RSS feed, add head link to rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export async function get(context: any) {
 	const posts = await getCollection('blog');
@@ -8,10 +12,13 @@ export async function get(context: any) {
 
 	const postItems = posts.map((post) => ({
 		...post.data,
+		// Note: this will not process components or JSX expressions in MDX files.
+		content: sanitizeHtml(parser.render(post.body)),
 		link: `/blog/${post.slug}/`,
 	}));
 	const noteItems = notes.map((note) => ({
 		...note.data,
+		content: sanitizeHtml(parser.render(note.body)),
 		link: `/notes/${note.slug}/`,
 	}));
 
